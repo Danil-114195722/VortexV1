e2cbcfeb88ca622b28fd03247a6735abb73ce942
sayasha <2two2two8eight@mail.ru>
2024-09-01 21:47:04 +0300

    adaptive

--- src/components/mainScreen/MainScreen.jsx
+++ src/components/mainScreen/MainScreen.jsx
@@ -16,6 +16,18 @@ const MainScreen = () => {
 				<div className="header__right">
 					<h5>+7 921 925 65 13</h5>
 					<input type="button" value='Оставить заявку'/>
+					<div className="menu">
+						<input type="checkbox" id='burger-checkbox' className='burger-checkbox' />
+						<label htmlFor="burger-checkbox" className='burger'></label>
+						<ul className='menu-list'>
+							<li><a href="" className="menu-item">Услуги</a></li>
+							<li><a href="" className="menu-item">Направления</a></li>
+							<li><a href="" className="menu-item">Преимущества</a></li>
+							<li><a href="" className="menu-item">Обратная связь</a></li>
+							<li><a href='' className="menu-item">+7 921 925 65 13</a></li>
+							<li><input type="button" value='Оставить заявку'/></li>
+						</ul>
+					</div>
 				</div>
 			</header>
 			<div className="entry">
